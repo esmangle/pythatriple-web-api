f25d9e0d06a454fc64f5b598698ad184d9a920a1
esmangle <esmendaangelo@gmail.com>
2025-09-20 01:03:46 +0800

    add service logic

--- pythatriple-web-api/src/main/java/com/example/pythatriple_web_api/service/PythatripleService.java
+++ pythatriple-web-api/src/main/java/com/example/pythatriple_web_api/service/PythatripleService.java
@@ -7,6 +7,28 @@ import com.example.pythatriple_web_api.dto.PythatripleResponse;
 @Service
 public class PythatripleService {
 	public PythatripleResponse getTriples(int hypotSq) {
+		return calculateTriples(hypotSq);
+	}
+
+	private PythatripleResponse calculateTriples(int hypotSq) {
+		int c = (int) Math.sqrt(hypotSq);
+
+		for (int a = 1; a < c; a++) {
+			int bSq = hypotSq - (a * a);
+
+			double bDouble = Math.sqrt(bSq);
+
+			if (bDouble % 1.0 == 0.0) {
+				int b = (int) bDouble;
+
+				double avg = (a + b + c) / 3.0;
+
+				return new PythatripleResponse(
+					a, b, c, avg
+				);
+			}
+		}
+
 		return null;
 	}
 }
