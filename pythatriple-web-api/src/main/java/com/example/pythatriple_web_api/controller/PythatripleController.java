43f3c874ba94796143dc09bce6e2683a3cd12382
esmangle <esmendaangelo@gmail.com>
2025-09-19 23:06:38 +0800

    add service

--- pythatriple-web-api/src/main/java/com/example/pythatriple_web_api/controller/PythatripleController.java
+++ pythatriple-web-api/src/main/java/com/example/pythatriple_web_api/controller/PythatripleController.java
@@ -2,19 +2,30 @@ package com.example.pythatriple_web_api.controller;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.pythatriple_web_api.dto.PythatripleResponse;
+import com.example.pythatriple_web_api.service.PythatripleService;
+
 
 @RestController
 @RequestMapping("/api")
 public class PythatripleController {
 
+	@Autowired
+	private PythatripleService service;
+
 	@GetMapping("triples")
 	public ResponseEntity<?> getTriples(@RequestParam Integer value) {
-		return ResponseEntity.ok().body(Map.of("test", "message"));
+		PythatripleResponse result = service.calculateTriples(value);
+
+		return ResponseEntity.ok().body(
+			(result == null) ? Map.of() : result
+		);
 	}
 }
---
+++ pythatriple-web-api/src/main/java/com/example/pythatriple_web_api/service/PythatripleService.java
@@ -0,0 +1,12 @@
+package com.example.pythatriple_web_api.service;
+
+import org.springframework.stereotype.Service;
+
+import com.example.pythatriple_web_api.dto.PythatripleResponse;
+
+@Service
+public class PythatripleService {
+	private PythatripleResponse getTriples(int value) {
+		return null;
+	}
+}
