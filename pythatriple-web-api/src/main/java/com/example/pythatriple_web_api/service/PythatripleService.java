83e17e17db22243ea64e4f7f46cb0d54fa188b5d
esmangle <esmendaangelo@gmail.com>
2025-09-20 01:23:30 +0800

    optimise gettriples logic

--- pythatriple-web-api/src/main/java/com/example/pythatriple_web_api/service/PythatripleService.java
+++ pythatriple-web-api/src/main/java/com/example/pythatriple_web_api/service/PythatripleService.java
@@ -11,22 +11,40 @@ public class PythatripleService {
 	}
 
 	private PythatripleResponse calculateTriples(int hypotSq) {
-		int c = (int) Math.sqrt(hypotSq);
+		double cDouble = Math.sqrt(hypotSq);
 
-		for (int a = 1; a < c; a++) {
+		int c = (int) Math.round(cDouble);
+
+		if (c * c != hypotSq) {
+			return null;
+		}
+
+		int aMax = (int) (c / Math.sqrt(2));
+
+		for (int a = 1; a <= aMax; a++) {
 			int bSq = hypotSq - (a * a);
+
+			if (bSq <= 0) {
+				continue;
+			}
 
 			double bDouble = Math.sqrt(bSq);
 
-			if (bDouble % 1.0 == 0.0) {
-				int b = (int) bDouble;
+			int b = (int) Math.round(bDouble);
 
-				double avg = (a + b + c) / 3.0;
-
-				return new PythatripleResponse(
-					a, b, c, avg
-				);
+			if (b * b != bSq) {
+				continue;
 			}
+
+			if (a >= b) {
+				continue;
+			}
+
+			double avg = (a + b + c) / 3.0;
+
+			return new PythatripleResponse(
+				a, b, c, avg
+			);
 		}
 
 		return null;
