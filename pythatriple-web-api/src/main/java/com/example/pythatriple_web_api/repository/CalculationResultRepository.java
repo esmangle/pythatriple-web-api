6cb209789f7e94a7213cea1f69ab78e910defab2
esmangle <esmendaangelo@gmail.com>
2025-09-22 13:15:23 +0800

    optimise queries with joins

--- pythatriple-web-api/src/main/java/com/example/pythatriple_web_api/repository/CalculationResultRepository.java
+++ pythatriple-web-api/src/main/java/com/example/pythatriple_web_api/repository/CalculationResultRepository.java
@@ -3,11 +3,16 @@ package com.example.pythatriple_web_api.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.pythatriple_web_api.model.CalculationResult;
 
+@Repository
 public interface CalculationResultRepository extends JpaRepository<CalculationResult, Long> {
+	@EntityGraph(attributePaths = {"triple"})
 	Optional<CalculationResult> findByHypotSq(Integer hypotSq);
+	@EntityGraph(attributePaths = {"triple"})
 	List<CalculationResult> findAllByOrderByTimestampDesc();
 }
