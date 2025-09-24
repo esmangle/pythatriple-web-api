b7cf44c7da878259b475b95c6672d3cfc10abb40
esmangle <esmendaangelo@gmail.com>
2025-09-25 03:03:54 +0800

    convert dto to kotlin

--- pythatriple-web-api/src/main/java/com/example/pythatriple_web_api/dto/PythatripleTableResponse.java
+++
@@ -1,5 +0,0 @@
-package com.example.pythatriple_web_api.dto;
-
-public record PythatripleTableResponse(
-	int hypotSq, int a, int b, int c, double avg
-) {}
---
+++ pythatriple-web-api/src/main/kotlin/com/example/pythatriple_web_api/dto/PythatripleTableResponse.kt
@@ -0,0 +1,9 @@
+package com.example.pythatriple_web_api.dto
+
+data class PythatripleTableResponse(
+	val hypotSq: Int,
+	val a: Int,
+	val b: Int,
+	val c: Int,
+	val avg: Double
+)
