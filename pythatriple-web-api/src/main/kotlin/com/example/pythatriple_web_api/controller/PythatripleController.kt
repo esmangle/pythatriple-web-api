c95361c60336c9b68b4192cf454cde8aaac728e8
esmangle <esmendaangelo@gmail.com>
2025-09-25 02:51:20 +0800

    convert controller to kotlin

---
+++ pythatriple-web-api/src/main/kotlin/com/example/pythatriple_web_api/controller/PythatripleController.kt
@@ -0,0 +1,43 @@
+package com.example.pythatriple_web_api.controller
+
+import com.example.pythatriple_web_api.dto.PythatripleRequest
+import com.example.pythatriple_web_api.service.PythatripleService
+import jakarta.validation.Valid
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.BindingResult
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
+class PythatripleController(
+	private val service: PythatripleService
+) {
+
+	@GetMapping("triples")
+	fun getAllTriples(): ResponseEntity<*> {
+		return ResponseEntity.ok(service.allTriples)
+	}
+
+	@GetMapping("triples", params = ["hypotenuse_squared"])
+	fun getTriple(
+		@Valid @ModelAttribute req: PythatripleRequest,
+		br: BindingResult
+	): ResponseEntity<*>  {
+		if (br.hasErrors()) {
+			val errors = br.fieldErrors.associate {
+				it.field to it.defaultMessage
+			}
+
+			return ResponseEntity.badRequest().body(errors)
+		}
+
+		val resp = service.getTriple(req.hypotenuse_squared)
+
+		return ResponseEntity.ok().body(
+			if (resp.isPresent) resp.get() else emptyMap<Any, Any>()
+		)
+	}
+}
---
+++ pythatriple-web-api/src/main/kotlin/com/example/pythatriple_web_api/dto/PythatripleRequest.kt
@@ -0,0 +1,10 @@
+package com.example.pythatriple_web_api.dto
+
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Positive
+
+data class PythatripleRequest(
+	@field:NotNull(message = "parameter is required")
+	@field:Positive(message = "must be a positive integer")
+	val hypotenuse_squared: Int
+)
